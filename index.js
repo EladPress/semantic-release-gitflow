60fc668c6fca24b398de7489ad476ffbc20c7dd4
EladPress <presselad2@gmail.com>
2023-11-24 00:11:17 +0200

    fix: testing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {version} from './package.json' assert { type: "json" };
+// import {version} from './package.json' assert { type: "json" };
 
 /**
  * Determine the type of release to create based on a list of commits.
@@ -19,7 +19,7 @@ export async function analyzeCommits(pluginConfig, context) {
     const { commits, logger } = context;
     logger.log(commits)
 
-    logger.log('semantic-release-gitflow@' + version);
+    // logger.log('semantic-release-gitflow@' + version);
     logger.log("pluginConfig: " + JSON.stringify(pluginConfig));
     logger.log("release rules: " + JSON.stringify(pluginConfig.releaseRules));
 }
